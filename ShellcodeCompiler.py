18ddc5b4a2b05394a9aec4b2f3e511aab188e15a
Patrick Lee <an20421@bristol.ac.uk>
2023-12-05 09:43:51 +0000

    proc mem issue

--- ShellcodeCompiler.py
+++ ShellcodeCompiler.py
@@ -9,6 +9,7 @@ def disassemble_shellcode(shellcode):
         print("0x%x:\t%s\t%s" % (instruction.address, instruction.mnemonic, instruction.op_str))
 
 shellcode = b"\x31\xc0\x50\x68\x2f\x2f\x73\x68\x68\x2f\x62\x69\x6e\x89\xe3\x50\x53\x89\xe1\xb0\x0b\xcd\x80"
+shellcode = b"\x6a\x19\x58\x99\x52\x89\xe3\xcd\x80\x40\xcd\x80"
 
 # Disassemble the shellcode
 disassemble_shellcode(shellcode)
